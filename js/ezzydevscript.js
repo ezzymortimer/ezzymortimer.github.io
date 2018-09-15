9d02eb60742649441c4854c6de6ced00fcc0b9fa
Ezzy Mortimer <34223272+ezzymortimer@users.noreply.github.com>
2018-09-15 14:52:53 +1200

    Update ezydevscript 15-09-18

--- js/ezzydevscript.js
+++ js/ezzydevscript.js
@@ -19,3 +19,8 @@ function delay(){
 		setTimeout(toggle_hide, 0);
 		var extended = 0;}
 }
+
+function togglecomment(e)
+{
+	var t=$(e).thing(),n=t.find(".expand:first"),r=t.hasClass("collapsed");t.toggleClass("collapsed noncollapsed"),r?n.text("[â€“]"):n.text("[+]");
+	}
